8c934b58eaf044b5b9c24b405d8ef925562e17c3
kncljmm <orangecalvin@naver.com>
2023-02-09 16:36:43 +0900

    B com V5

--- src/oop/Test2.java
+++ src/oop/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("깃연습입니다......");
 		System.out.println("희원이 바보입니다...");
+		
+		System.err.println("현아 개발자가 변경한 내용입니다..");
 	}
 
 }
